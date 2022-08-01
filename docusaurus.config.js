aecc3acb27300e3a866f2aabd6687ffb1227273f
Zeffon Wu <ZeffonWu@gmail.com>
2022-08-01 17:58:28 +0800

    chore: add algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,117 +1,119 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://zeffon.github.io',
-  baseUrl: '/',
-  projectName: 'zeffon.github.io',
-  organizationName: 'zeffon',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+(
+  module.exports = {
+    title: "My Site",
+    tagline: "Dinosaurs are cool",
+    url: "https://zeffon.github.io",
+    baseUrl: "/",
+    projectName: "zeffon.github.io",
+    organizationName: "zeffon",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            // Please change this to your repo.
+            editUrl: "https://github.com/zeffon/zeffon.github.io/tree/master",
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl: "https://github.com/zeffon/zeffon.github.io/blob/master",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/zeffon/zeffon.github.io/tree/master',
+        navbar: {
+          title: "首页",
+          logo: {
+            alt: "My Site Logo",
+            src: "img/logo.svg",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "文档",
+            },
+            { to: "/blog", label: "博客", position: "left" },
+            {
+              href: "https://www.yuque.com/zeffon/blog",
+              label: "语雀",
+              position: "right",
+            },
+            {
+              href: "https://github.com/zeffon",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/zeffon/zeffon.github.io/blob/master',
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "文档",
+                  to: "/docs/intro",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "ZeffonWu@gmail.com",
+                  href: "mailto:ZeffonWu@gmail.com",
+                },
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "语雀",
+                  href: "https://www.yuque.com/zeffon/blog",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Zeffon, Inc. Built with Docusaurus.`,
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+        },
+        algolia: {
+          appId: "1A7I0HQZDA",
+          apiKey: "31a8c0510c8ecc49770cffd07aac3944",
+          indexName: "zeffon",
+          // Optional: see doc section below
+          contextualSearch: true,
+          // Optional: see doc section below
+          // Optional: Algolia search parameters
+          searchParameters: {},
+          //... other Algolia params
         },
       }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: '首页',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: '文档',
-          },
-          {to: '/blog', label: '博客', position: 'left'},
-          {
-            href: 'https://www.yuque.com/zeffon/blog',
-            label: '语雀',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/zeffon',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: '文档',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'ZeffonWu@gmail.com',
-                href: 'mailto:ZeffonWu@gmail.com',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: '语雀',
-                href: 'https://www.yuque.com/zeffon/blog',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Zeffon, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-      algolia: {
-        apiKey: 'YOUR_API_KEY',
-        indexName: 'YOUR_INDEX_NAME',
-        // Optional: see doc section below
-        contextualSearch: true,
-        // Optional: see doc section below
-        appId: 'YOUR_APP_ID',
-        // Optional: Algolia search parameters
-        searchParameters: {},
-        //... other Algolia params
-      },
-    }),
-});
+  }
+);
